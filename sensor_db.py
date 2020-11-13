434663f67ae31df899f0552f78d08bbb56c3c0a4
TrevorByler <trevordbyler@gmail.com>
2020-11-13 13:31:07 -0500

    add sensor_db.py

---
+++ sensor_db.py
@@ -0,0 +1,54 @@
+import time
+import mysql.connector
+
+import board
+import busio
+import adafruit_tsl2591
+
+# Initialize the I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Initialize the sensor.
+sensor = adafruit_tsl2591.TSL2591(i2c)
+
+# Initialize MySQL db connection
+mydb = mysql.connector.connect(
+    host= "localhost",
+    user= "root",
+    password= "logic",
+    database= "LightData"
+    )
+mycursor = mydb.cursor()
+
+sql_delete = "DELETE FROM raw_data WHERE time < CURRENT_TIMESTAMP - INTERVAL 5 MINUTE"
+# condition = ("CURRENT_TIMESTAMP - INTERVAL 5 MINUTE",)
+mycursor.execute(sql_delete)
+mydb.commit()
+print(mycursor.rowcount, "record(s) deleted")
+
+# normalizes output to approximately the percentage of max light
+n_factor  = 5.825/2147483647.0
+count = 0
+last_del = time.time()
+
+while True:
+    visible = sensor.visible*n_factor*100
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    sql = """INSERT INTO raw_data (time, visible_light)
+                VALUES (%s, %s)"""
+    vals = (timestamp, visible)
+    mycursor.execute(sql, vals)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted: {}".format(vals))
+    
+    if (time.time() - last_del)/60 > 5:
+        sql_delete = "DELETE FROM raw_data WHERE time < CURRENT_TIMESTAMP - INTERVAL 5 MINUTE"
+        mycursor.execute(sql_delete)
+        mydb.commit()
+        print(mycursor.rowcount, "record(s) deleted")
+        last_del = time.time()
+        
+    count = count + 1
+    time.sleep(1)
+
